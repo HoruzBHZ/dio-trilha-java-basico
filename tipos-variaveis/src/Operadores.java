368740aac767584002ab7185e870b87fd36ee7f1
HoruzBHZ <renatomarques1908@gmail.com>
2024-01-09 10:51:06 -0300

    criando o projeto para exemplo de tipos e variaveis

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,10 +1,10 @@
 public class Operadores {
     public static void main(String[] args) {
     int a, b;
-    a = 5;
+    a = 6;
     b = 6;
 
-    String resultado = a==b ? "verdadeiro" : "falso";
+    int resultado = a == b ? 1 : 0;
 
     System.out.println(resultado);
     
